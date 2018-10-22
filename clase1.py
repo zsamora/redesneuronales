4a9ad4459954c59331ae57a91010ada82bc63acc
Benja <zsamora@ug.uchile.cl>
2018-10-22 15:30:44 -0300

    Redes Neuronales v1

--- clase1.py
+++ clase1.py
@@ -2,46 +2,124 @@ import numpy as np
 import unittest
 import matplotlib.pyplot as plt
 import random
+import math
 
-class Perceptron:
-    def __init__(self, w, b, lr=0):
+class AbstractNeuron:
+    def __init__(self, w, b, lr=0, precision = 0, nlayer = 0):
         self.weight = w # Vector of weights
         self.bias = b   # Bias
-        self.lrate = lr # Learning rate
+        self.lr = lr # Learning rate
+        self.precision = precision
+        self.nlayer = nlayer
 
+    def train(self, point, expected):
+        real = self.feed(point) # Perceptron's output according to w and b
+        if (expected != real):
+            diff = expected - real
+            newweight = self.weight + (self.lr * point * diff) # Este se puede repetir en todos los input en vez de realizarlo solo una vez)
+            self.weight = newweight
+            newbias = self.bias + (self.lr * diff)
+            self.bias = newbias
+    def plotpoints(self):
+        tpoints = input('Ingrese la cantidad de puntos para entrenar: ')
+        tpoints = int(tpoints)
+        npoints = input('Ingrese la cantidad de puntos a graficar: ')
+        npoints = int(npoints)
+        trainingpoints = np.random.rand(tpoints,2)
+        for t in trainingpoints:
+            if t[0] < t[1]: # Recta y = x
+                self.train(t,0)
+            else:
+                self.train(t,1)
+        ## Clasificacion de los puntos reales
+        newpoints = np.random.rand(npoints,2)
+        listclassification = []
+        for n in newpoints:
+            if n[0] < n[1]: # Recta y = x
+                listclassification.append(0)
+            else:
+                listclassification.append(1)
+        output = []
+        for po in range(0,len(newpoints)):
+            classification = self.feed(newpoints[po])
+            output.append(classification)
+            if classification:
+                plt.scatter(newpoints[po][0],newpoints[po][1],c='red')
+            else:
+                plt.scatter(newpoints[po][0],newpoints[po][1],c='blue')
+        plt.plot([0,1], [0,1],c='black')
+        aciertos = 0
+        for l,o in zip(listclassification,output):
+            aciertos += 1 if (l == o) else 0
+        precision = aciertos / len(output)
+        print(precision)
+        plt.show()
+    def plotlearning(self):
+        p = []
+        npoints = input('Ingrese la cantidad de puntos a graficar: ')
+        npoints = int(npoints)
+        for i in range (0,500):
+            p.append(self.precisionlearning(i,npoints))
+        plt.plot(range(0,500),p)
+        plt.show()
+
+    def precisionlearning(self, tpoints, npoints):
+        trainingpoints = np.random.rand(tpoints,2)
+        for t in trainingpoints:
+            if t[0] < t[1]: # Recta y = x
+                self.train(t,0)
+            else:
+                self.train(t,1)
+        ## Clasificacion de los puntos reales
+        newpoints = np.random.rand(npoints,2)
+        listclassification = []
+        for n in newpoints:
+            if n[0] < n[1]: # Recta y = x
+                listclassification.append(0)
+            else:
+                listclassification.append(1)
+        output = []
+        for po in range(0,len(newpoints)):
+            classification = self.feed(newpoints[po])
+            output.append(classification)
+        aciertos = 0
+        for l,o in zip(listclassification,output):
+            aciertos += 1 if (l == o) else 0
+        precision = aciertos / len(output)
+        return precision
+
+class Perceptron(AbstractNeuron):
     def feed(self, x):
         result = np.dot(self.weight, x) + self.bias
         if (result <= 0):
             return 0
         else:
             return 1
+class Sigmoid(AbstractNeuron):
+    def feed(self, x):
+        result = 1 / (1 + math.exp(-1 * np.dot(self.weight, x) - self.bias))
+        return result
+class NeuralNetwork():
+class NeuronLayer():
+    def feed(self, x):
+        array = []
+        for y in x:
+            array.append(y.feed(x))
+        if (self.nlayer == self.getnlayer):
+            
 
-    def train(self, point, value):
-        output = self.feed(point) # Perceptron's output according to w and b
-        if (output != value):
-            diff = value - output
-            newweight = self.weight + (self.lr * point * diff) # Este se puede repetir en todos los input en vez de realizarlo solo una vez
-            self.weight = newweight
-            newbias = self.bias + (self.lr * diff)
-            self.bias = newbias
-
-    def plot(self, listpoint, listvalues, newpoints):
-        for point,value in zip(listpoint,listvalues):
-            self.train(point,value)
+class Neuron():
 
 
 class AndPerceptron(Perceptron):
     def __init__(self):
         Perceptron.__init__(self, np.array([1,0]), -0.5)
-
 class OrPerceptron(Perceptron):
     def __init__(self):
         Perceptron.__init__(self, np.array([1,1]), -1.5)
-
 class NandPerceptron(Perceptron):
     def __init__(self):
         Perceptron.__init__(self, np.array([-2,-2]), 3)
-
 class SumPerceptron(Perceptron):
     def __init__(self):
         self.nand = NandPerceptron()
@@ -54,10 +132,13 @@ class SumPerceptron(Perceptron):
         return np.array([rescarry,ressum])
 class TrainPerceptron(Perceptron):
     def __init__(self):
-        Perceptron.__init__(self, np.array([random.randint(-2,2),random.randint(-2,2)]), random.randint(-2,2))
+        Perceptron.__init__(self, np.array([random.randint(-2,2),random.randint(-2,2)]), random.randint(-2,2),0.1)
+class TrainSigmoid(Perceptron):
+    def __init__(self):
+        Sigmoid.__init__(self, np.array([random.randint(-2,2),random.randint(-2,2)]), random.randint(-2,2),0.1)
 
 def main():
-    choice = input('Seleccione operacion (AND=1, OR=2, NAND=3, SUM=4, TRAIN=5):')
+    choice = input('Seleccione operacion (AND=1, OR=2, NAND=3, SUM=4, TRAINPERCEPTRON=5, TRAINSIGMOID=6, PRECISIONPERCEPTRON=7, PRECISIONTRAINSIGMOID=8):')
     choice = int(choice)
     if (choice == 1):
         p = AndPerceptron()
@@ -69,26 +150,20 @@ def main():
         p = SumPerceptron()
     if (choice == 5):
         p = TrainPerceptron()
-        #trainingpoints = input('Ingrese valores: ')
-        #trainingvalues = input('Ingrese clasificaciones: ')
-        tpoints = input('Ingrese la cantidad de puntos para entrenar: ')
-        tpoints = int(tpoints)
-        npoints = input('Ingrese la cantidad de puntos a graficar: ')
-        npoints = int(npoints)
-        #trainingpoints = np.fromstring(trainingpoints, dtype=int, sep=' ')
-        #print(trainingpoints)
-        #trainingvalues = np.fromstring(trainingvalues, dtype=int, sep=' ')
-        trainingpoints = np.random.rand(tpoints,2)
-        trainingvalues = np.array([])
-        for t in trainingpoints:
-            if
-            np.insert(trainingvalues, )
-        newpoints = np.random.rand(npoints,2)
-        p.plot(trainingpoints, trainingvalues, newpoints)
-    x1 = input('Ingrese x1: ')
-    x2 = input('Ingrese x2: ')
-    result = p.feed(np.array([int(x1),int(x2)]))
-    print(result)
+        p.plotpoints()
+    if (choice == 6):
+        p = TrainSigmoid()
+        p.plotpoints()
+    if (choice == 7):
+        p = TrainPerceptron()
+        p.plotlearning()
+    if (choice == 8):
+        p = TrainSigmoid()
+        p.plotlearning()
+    #x1 = input('Ingrese x1: ')
+    #x2 = input('Ingrese x2: ')
+    #result = p.feed(np.array([int(x1),int(x2)]))
+    #print(result)
 
 if __name__ == "__main__":
     main()
